d640bbe9054d1075ed7dd64000fbc05f4ba5b50f
monklof <monklof@gmail.com>
2016-01-19 12:18:02 -0500

    intro page

--- robot/main.lisp
+++ robot/main.lisp
@@ -1,10 +1,10 @@
 (defun myserver ()      
   (start (make-instance 'easy-acceptor :port 7777))
   (define-easy-handler (greet :uri "/") ()
-    (format nil "<html><body><h1>Of course this is a robot :)</h1><footer> - powered by lisp </footer></body></html>")))
+    (format nil "<html><body><h1><a href=\"https://github.com/monklof/robot\">A robot that help me dig up informations in the world that I should like :)</a></h1><footer>Powered by Lisp </footer></body></html>")))
 
-(ql:quickload "hunchentoot")
-(use-package :hunchentoot)
-(load "~/robot/robot/main.lisp")
-(myserver)
+;;(ql:quickload "hunchentoot")
+;;(use-package :hunchentoot)
+;;(load "~/robot/robot/main.lisp")
+;;(myserver)
 
